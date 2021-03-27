5e4d3edcbf02d9d858912fd006a45d370e1e5bbd
Mathvivas <mathvivas@hotmail.com>
2021-03-27 11:01:33 -0300

    Criação do Primeiro Componente

---
+++ introducao-componentes/src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ola-angular',          // Nome da Tag HTML
+    template: `
+        <p>Olá, Angular!</p>
+    `
+})
+export class OlaAngularComponent {
+}
