79aa5c7df0fdf1a8bd659279de377f8cbc4569f7
Mathvivas <mathvivas@hotmail.com>
2021-03-27 12:08:29 -0300

    Update e Bootstrap

--- introducao-componentes/src/app/ola-angular-cli/ola-angular-cli.component.ts
+++ introducao-componentes/src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// ng generate component ola-angular-cli
 
 @Component({
   selector: 'app-ola-angular-cli',
   templateUrl: './ola-angular-cli.component.html',
   styleUrls: ['./ola-angular-cli.component.css']
 })
-
-// ng generate component ola-angular-cli
-
-export class OlaAngularCliComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class OlaAngularCliComponent {
 
 }
