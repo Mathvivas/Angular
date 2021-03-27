324d561ab4b4c935f06f697a2d1e4a7e2d458958
Mathvivas <mathvivas@hotmail.com>
2021-03-27 11:20:41 -0300

    Criação do html e css do primeiro componente

--- introducao-componentes/src/app/ola-angular/ola-angular.component.ts
+++ introducao-componentes/src/app/ola-angular/ola-angular.component.ts
@@ -2,9 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'ola-angular',          // Nome da Tag HTML
-    template: `
-        <p>Olá, Angular!</p>
-    `
+    templateUrl: './ola-angular.component.html',
+    styleUrls: [
+        './ola-angular.component.css'
+    ]
 })
 export class OlaAngularComponent {
+    framework: string = 'Angular'
 }
