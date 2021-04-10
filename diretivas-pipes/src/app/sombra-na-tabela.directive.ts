718faf0e24c859345bf2fc63fea467e5183623de
Mathvivas <mathvivas@hotmail.com>
2021-04-10 12:27:40 -0300

    Criaçao de uma Diretiva

--- diretivas-pipes/src/app/sombra-na-tabela.directive.ts
+++ diretivas-pipes/src/app/sombra-na-tabela.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSombraNaTabela]'
 })
 export class SombraNaTabelaDirective {
 
-  constructor() { }
+  // Injeção de Dependência
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) { 
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'box-shadow', '10px 10px'
+    )
+   }
 
 }
