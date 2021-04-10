6b188dc40c2c1ccaaf846cd3e0454b8fc1a34dd3
Mathvivas <mathvivas@hotmail.com>
2021-04-10 12:43:04 -0300

    Criaçao de Estilos

--- diretivas-pipes/src/app/sombra-na-tabela.directive.ts
+++ diretivas-pipes/src/app/sombra-na-tabela.directive.ts
@@ -1,19 +1,34 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSombraNaTabela]'
+  //selector: 'table'     // Selector por tipo, aplica emm todas as tabelas automaticamente
 })
 export class SombraNaTabelaDirective {
+
+  @HostListener('mouseover') quandoOMousePassarPorCima (): void {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'box-shadow', '10px 10px'
+    )
+  }
+
+  @HostListener('mouseleave') quandoOMouseSair (): void {
+    this.renderer.removeStyle(
+      this.elementRef.nativeElement,
+      'box-shadow'
+    )
+  }
 
   // Injeção de Dependência
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
   ) { 
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'box-shadow', '10px 10px'
-    )
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'box-shadow', '10px 10px'
+    // )
    }
 
 }
