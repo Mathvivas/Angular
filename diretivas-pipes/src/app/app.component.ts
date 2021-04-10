7155b1cb48953c979c7cd7d6916d4d9481b8ba2c
Mathvivas <mathvivas@hotmail.com>
2021-04-10 12:17:27 -0300

    Criaçao do Projeto

---
+++ diretivas-pipes/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  lembrete!: string
+  lembretes: Array<string> = []
+
+  salvar() {
+    // spread operator
+    this.lembretes = [this.lembrete, ...this.lembretes]
+    this.lembrete = "";
+  }
+}
