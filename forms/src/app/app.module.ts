a93f0a94b91e71561f3d93457f120208b0d60082
Mathvivas <mathvivas@hotmail.com>
2021-04-10 10:17:54 -0300

    Criaçao do Forms

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
