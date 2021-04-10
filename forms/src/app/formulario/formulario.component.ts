71c4986eb87f5f39aa8b96cbad9e5e5858d08712
Mathvivas <mathvivas@hotmail.com>
2021-04-10 11:07:23 -0300

    Melhoria do Forms com ngForm

--- forms/src/app/formulario/formulario.component.ts
+++ forms/src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+
+  profissoes: string[] = [
+    'Engenheiro',
+    'Professor',
+    'Químico',
+    'Zoólogo',
+    'Advogado'
+  ]
+
+  salvar(pessoaForm: NgForm): void {
+    const nome = pessoaForm.value.nome
+    const fone = pessoaForm.value.fone
+    const email = pessoaForm.value.email
+    const profissao = pessoaForm.value.profissao
+    console.log(`
+      Nome: ${nome},
+      Telefone: ${fone},
+      Email: ${email},
+      Profissão: ${profissao}
+      `)
+  }
 
   constructor() { }
 
